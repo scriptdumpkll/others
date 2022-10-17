89dfa9a738141b65a96e16302d1752301abbaa17
scriptdumpkll <113224535+scriptdumpkll@users.noreply.github.com>
2022-10-17 16:25:25 +0800

    Create crasher.lua

---
+++ crasher.lua
@@ -0,0 +1,482 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+if game:service"CoreGui":FindFirstChild("JMANbyJared") then
+    game:service"CoreGui":FindFirstChild("JMANbyJared"):Destroy()
+end
+
+wait(1)
+
+if not game:GetService("Players").LocalPlayer:IsInGroup(15500191) then
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "JMAN CRASHER";
+        Text = "must join the group to start the crash, copied to clipboard";
+        Duration = 10;
+    })
+    setclipboard("https://www.roblox.com/groups/15500191/JM#!/about")
+    return
+end
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "JMAN CRASHER";
+    Text = "loading...";
+    Duration = 3;
+})
+repeat wait(0.001) until workspace.Players:FindFirstChild(game:service"Players".LocalPlayer.Name)
+game.StarterGui:SetCore("SendNotification", {
+    Title = "JMAN CRASHER";
+    Text = "loaded";
+    Duration = 3;
+})
+
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "JMAN CRASHER";
+    Text = "Made By jaredd#0995";
+    Icon = "http://www.roblox.com/asset/?id=11160325159";
+    Duration = 9e9;
+})
+
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local Panel = Instance.new("TextLabel")
+local Start = Instance.new("TextButton")
+local Pause = Instance.new("TextButton")
+local ForceCrash = Instance.new("TextButton")
+local Status = Instance.new("TextLabel")
+
+ScreenGui.Name = "JMANbyJared"
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(144, 238, 144)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.14567256, 0, 0.252044141, 0)
+Frame.Size = UDim2.new(0, 418, 0, 107)
+
+Panel.Name = "Panel"
+Panel.Parent = Frame
+Panel.BackgroundColor3 = Color3.fromRGB(228, 255, 205)
+Panel.BorderColor3 = Color3.fromRGB(27, 42, 53)
+Panel.BorderSizePixel = 0
+Panel.Size = UDim2.new(0, 417, 0, 36)
+Panel.Font = Enum.Font.Highway
+Panel.Text = "JMAN CRASHER"
+Panel.TextColor3 = Color3.fromRGB(0, 0, 0)
+Panel.TextSize = 30.000
+
+Start.Archivable = false
+Start.Name = "Start"
+Start.Parent = Frame
+Start.BackgroundColor3 = Color3.fromRGB(228, 255, 205)
+Start.BorderSizePixel = 0
+Start.Position = UDim2.new(0.0360671766, 0, 0.427497655, 0)
+Start.Size = UDim2.new(0, 80, 0, 48)
+Start.Font = Enum.Font.Cartoon
+Start.Text = "Start"
+Start.TextColor3 = Color3.fromRGB(0, 0, 0)
+Start.TextSize = 15.000
+
+Pause.Archivable = false
+Pause.Name = "Pause"
+Pause.Parent = Frame
+Pause.BackgroundColor3 = Color3.fromRGB(228, 255, 205)
+Pause.BorderSizePixel = 0
+Pause.Position = UDim2.new(0, 102, 0, 45)
+Pause.Size = UDim2.new(0, 80, 0, 48)
+Pause.Font = Enum.Font.Cartoon
+Pause.Text = "Pause"
+Pause.TextColor3 = Color3.fromRGB(0, 0, 0)
+Pause.TextSize = 15.000
+
+ForceCrash.Archivable = false
+ForceCrash.Name = "ForceCrash"
+ForceCrash.Parent = Frame
+ForceCrash.BackgroundColor3 = Color3.fromRGB(228, 255, 205)
+ForceCrash.BorderSizePixel = 0
+ForceCrash.Position = UDim2.new(0.456005901, 0, 0.424546421, 0)
+ForceCrash.Size = UDim2.new(0, 80, 0, 48)
+ForceCrash.Font = Enum.Font.Cartoon
+ForceCrash.Text = "Force Crash"
+ForceCrash.TextColor3 = Color3.fromRGB(0, 0, 0)
+ForceCrash.TextSize = 15.000
+
+local maxitems = 1400
+Status.Name = "Status"
+Status.Parent = Frame
+Status.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Status.BackgroundTransparency = 1.000
+Status.Position = UDim2.new(0.674342334, 0, 0.420560747, 0)
+Status.Size = UDim2.new(0, 122, 0, 48)
+Status.Font = Enum.Font.Cartoon
+Status.Text = "Status: 0 / "..tostring(maxitems).." \nPercent: 0%"
+Status.TextColor3 = Color3.fromRGB(0, 0, 0)
+Status.TextSize = 14.000
+
+local remotes = {
+    "CHECKER_1",
+    "CHECKER_2",
+    "TeleportDetect",
+    "OneMoreTime",
+    "BreathingHAMON",
+    "VirusCough",
+}
+
+local __namecall
+__namecall = hookmetamethod(game, "__namecall", function(...)
+    local args = {...}
+    local method = getnamecallmethod()
+    if (method == "FireServer" and args[1].Name == "MainEvent" and table.find(remotes, args[2])) then
+        return
+    end
+    return __namecall(table.unpack(args))
+end)
+
+local av1 = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+	av1:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+	wait(1)
+	av1:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
+Frame.Active = true
+Frame.Draggable = true
+
+local player = game:service"Players".LocalPlayer
+local ws = workspace
+local shop = ws.Ignored:FindFirstChild("Shop")
+
+player:GetMouse().KeyDown:Connect(function(key)
+    if key:lower() == "v" then
+        Frame.Visible = not Frame.Visible
+    end
+end)
+
+local Paused, Stopped, Running = false, false, false
+
+function notify(a,b,c)
+    game:service"StarterGui":SetCore("SendNotification",{
+        Title = a;
+        Text = b;
+        Duration = c;
+    })
+end
+
+local function GatherItems()
+    local plr = game:service"Players".LocalPlayer
+    local cnt = 0
+    for i,v in pairs(plr.Character:GetChildren()) do
+        if v:IsA("Tool") then
+            cnt = cnt + 1
+            if v:FindFirstChild("Handle") then
+                v:FindFirstChild("Handle").Transparency = 100
+            end
+        end
+    end
+    for i,v in pairs(plr.Backpack:GetChildren()) do
+        if v:IsA("Tool") then
+            cnt = cnt + 1
+            if v:FindFirstChild("Handle") then
+                v:FindFirstChild("Handle").Transparency = 100
+            end
+        end
+    end
+    return cnt
+end
+
+local ItemCnt = GatherItems()
+local old = 0
+coroutine.resume(coroutine.create(function()
+    while wait(1) do
+        if Running == true then
+            ItemCnt = GatherItems()
+            local tot = ItemCnt/maxitems*100
+            tot = tonumber(string.format("%.2f",tot))
+            Status.Text = "Progess: "..tostring(ItemCnt).."/"..tostring(maxitems).." \nPercent: "..tostring(tot).."%"
+        end
+    end
+end))
+
+local function uneqipall()
+    for i,v in pairs(player.Character:GetChildren()) do
+        if v:IsA("Tool") then
+            v.Parent = player.Backpack
+        end
+    end
+end
+
+
+local function equipall()
+    for i,v in pairs(player.Backpack:GetChildren()) do
+        if v:IsA("Tool") then
+            v.Parent = player.Character
+        end
+    end
+end
+
+local function uneqipall()
+    for i,v in pairs(player.Character:GetChildren()) do
+        if v:IsA("Tool") then
+            v.Parent = player.Backpack
+        end
+    end
+end
+
+local function check_grenade()
+    local cnt = 0
+    for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+        if v.Name == '[Grenade]' then
+            cnt = cnt + 1
+        end
+    end
+    for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v.Name == '[Grenade]' then
+            cnt = cnt + 1
+        end
+    end
+    return cnt
+end
+
+local function check_lockpicks()
+    local cnt = 0
+    for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+        if v.Name == '[LockPicker]' then
+            cnt = cnt + 1
+        end
+    end
+    for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v.Name == '[LockPicker]' then
+            cnt = cnt + 1
+        end
+    end
+    return cnt
+end
+
+local function DeleteChar()
+    for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+        if v:IsA('MeshPart') or v:IsA('Part') or v:IsA('Accessory') then
+            v:Destroy()
+        end
+    end
+    game.Players.LocalPlayer.Character.Name = 'Deleted'
+end
+
+local GoodOverride = false
+local oldcash = player.DataFolder.Currency.Value
+player.DataFolder.Currency.Changed:Connect(function()
+    if tonumber(player.DataFolder.Currency.Value) ~= tonumber(oldcash) then
+        oldcash = player.DataFolder.Currency.Value
+        GoodOverride = true
+    end
+end)
+
+Start.MouseButton1Down:Connect(function()
+    if game.Players.LocalPlayer.Character:FindFirstChild("BodyEffects"):FindFirstChild('Attacking') then
+        game.Players.LocalPlayer.Character:FindFirstChild("BodyEffects"):FindFirstChild('Attacking'):Destroy()
+    end
+
+    if Running == false then
+        notify("JMAN CRASHER","started",5)
+        notify("JMAN CRASHER","this will take awhile..",10)
+
+        Running = true
+        local Items = {
+            "[Flowers] - $5", "[Glock] - $500", "[Cranberry] - $3", "[TacticalShotgun] - $1750", 
+            "[P90] - $1000", "[AUG] - $1950", "[PepperSpray] - $75", "[Silencer] - $400", "[Donut] - $5", 
+            "[SMG] - $750", "[AR] - $1000", "[Shotgun] - $1250", "[Taser] - $1000", "[Weights] - $120", 
+            "[HeavyWeights] - $250", "[Popcorn] - $7", "[Camera] - $100", "[Chicken] - $7", "[Pizza] - $5", 
+            "[Flashlight] - $10", "[Bat] - $250", "[Lettuce] - $5", "[Pitchfork] - $320", "[Taco] - $2", 
+            "[Hamburger] - $5", "[Lemonade] - $3", "[SledgeHammer] - $350", "[LMG] - $3750", 
+            "[Starblox Latte] - $5", "[StopSign] - $300", "[SilencerAR] - $1250", "[AK47] - $2250", 
+            "[DrumGun] - $3000", "[Money Gun] - $777", "[Shovel] - $320", "[BrownBag] - $25", "[Shotgun] - $1250", 
+            "[Flashbang] - $550", "[Double-Barrel SG] - $1400", "[Revolver] - $1300", "[Donut] - $10", "[Pencil] - $175", 
+            "[Popcorn] - $14", "[Nunchucks] - $450", "[HotDog] - $8", "[Taser] - $1250", "[Key] - $125", 
+            "[Meat] - $12", "[Da Milk] - $7", "[Rifle] - $1550", 
+        }
+        uneqipall()
+        repeat
+            if Paused == true then
+                repeat wait(0.001) until Paused == false
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game:GetService("Workspace").Ignored.Shop["[Grenade] - $700"].Head.Position.X, game:GetService("Workspace").Ignored.Shop["[Grenade] - $700"].Head.Position.Y - 7, game:GetService("Workspace").Ignored.Shop["[Grenade] - $700"].Head.Position.Z)
+            fireclickdetector(game.Workspace.Ignored.Shop["[Grenade] - $700"].ClickDetector)
+            task.wait()
+        until check_grenade() >= 11
+        repeat
+            if Paused == true then
+                repeat wait(0.001) until Paused == false
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game:GetService("Workspace").Ignored.Shop["[LockPicker] - $100"].Head.Position.X, game:GetService("Workspace").Ignored.Shop["[LockPicker] - $100"].Head.Position.Y - 7, game:GetService("Workspace").Ignored.Shop["[LockPicker] - $100"].Head.Position.Z)
+            fireclickdetector(game.Workspace.Ignored.Shop["[LockPicker] - $100"].ClickDetector)
+            task.wait()
+        until check_lockpicks() >= 11
+        local function equipallcans()
+            for i,v in pairs(player.Backpack:GetChildren()) do
+                if v:IsA("Tool") and v.Name == "[SprayCan]" then
+                    v.Parent = player.Character
+                end
+            end
+        end
+
+        game:service"RunService":BindToRenderStep("RUNNNER_1",1,function()
+            --equipallcans()
+            pcall(function()
+                game:GetService("Players").LocalPlayer.PlayerGui.MainScreenGui.Crew.CrewFrame.Visible = false
+            end)
+            pcall(function()
+                game:GetService("Players").LocalPlayer.PlayerGui.GoThruGui.RecordingFrame.Visible = false
+            end)
+            pcall(function()
+                game:GetService("Players").LocalPlayer.PlayerGui.MainScreenGui.PhoneCalls.Visible = false
+            end)
+            game:service"ReplicatedStorage".MainEvent:FireServer("JoinCrew", 15500191)
+        end)
+        
+        for i,v in pairs(Items) do
+            equipall()
+            local place = shop:FindFirstChild(v)
+            GoodOverride = false
+            if Paused == true then
+                repeat wait(0.001) until Paused == false
+            end
+            repeat
+                player.Character.HumanoidRootPart.CFrame = CFrame.new(place.Head.Position.X, place.Head.Position.Y - 6, place.Head.Position.Z)
+                fireclickdetector(place.ClickDetector)
+                task.wait()
+            until GoodOverride == true
+        end
+        for i,v in pairs(Items) do
+            equipall()
+            local place = shop:FindFirstChild(v)
+            GoodOverride = false
+            if Paused == true then
+                repeat wait(0.001) until Paused == false
+            end
+            repeat
+                player.Character.HumanoidRootPart.CFrame = CFrame.new(place.Head.Position.X, place.Head.Position.Y - 6, place.Head.Position.Z)
+                fireclickdetector(place.ClickDetector)
+                task.wait()
+            until GoodOverride == true
+        end
+        uneqipall()
+        game:service"RunService":BindToRenderStep("CANNER",1,function()
+            equipallcans()
+        end)
+
+        repeat
+            --equipall()
+            if Paused == true then
+                repeat wait(0.001) until Paused == false
+            end
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game:GetService("Workspace").Ignored.Shop["[Flowers] - $5"].Head.Position.X, game:GetService("Workspace").Ignored.Shop["[Flowers] - $5"].Head.Position.Y - 5, game:GetService("Workspace").Ignored.Shop["[Flowers] - $5"].Head.Position.Z)
+            fireclickdetector(game:GetService("Workspace").Ignored.Shop["[Flowers] - $5"].ClickDetector)
+            Count = 0 
+            for i,v in pairs(game:GetService("Workspace").Ignored.ItemsDrop:GetDescendants()) do
+                local s,e = pcall(function()
+                    if v:IsA('Tool') then
+                        repeat 
+                            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Parent.Position)  
+                            task.wait()
+                        until not v
+                    end
+                end) if e then print(e) end
+            end
+            for i,v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
+                if v:IsA('Tool') then
+                    Count = Count + 1
+                end
+            end
+            for i,v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+                if v:IsA('Tool') then
+                    Count = Count + 1
+                end   
+            end
+            task.wait()
+        until ItemCnt >= maxitems
+        pcall(function() game:service"RunService":UnBindToRenderStep("CANNER") end)
+        equipall()
+        if ItemCnt >= maxitems then
+            equipall()
+            local Player = game.Players.LocalPlayer
+
+            Player.Character.HumanoidRootPart.CFrame = CFrame.new(0,99999999,0)
+            local Lit = false
+            coroutine.resume(coroutine.create(function()
+                while not Lit do 
+                    game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+                    task.wait()
+                end
+            end))
+            for i,v in pairs(Player.Backpack:GetChildren()) do
+                if v:IsA('Tool') then
+                    v.Parent = Player.Character
+                end
+            end
+            
+            local T = 0
+            repeat
+                for i,v in pairs(Player.Backpack:GetChildren()) do
+                    if v:IsA('Tool') then
+                        v.Parent = Player.Character
+                    end
+                end
+                T = T + 1
+            until T >= 35
+ 
+            wait(3)
+            Lit = true
+            DeleteChar()
+        end
+    else
+        game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, true)
+        notify("JMAN CRASHER", "already started",5)
+    end
+end)
+
+Pause.MouseButton1Click:Connect(function()
+    Paused = not Paused
+    if Paused then
+        Pause.Text = "Paused"
+        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 8, 0)
+        notify("JMAN CRASHER","paused",3)
+    else
+        Pause.Text = "Resumed"
+        notify("JMAN CRASHER", "resumed",3)
+    end
+end)
+
+ForceCrash.MouseButton1Down:Connect(function()
+local Player = game.Players.LocalPlayer
+
+Player.Character.HumanoidRootPart.CFrame = CFrame.new(0,99999999,0)
+local Lit = false
+coroutine.resume(coroutine.create(function()
+    while not Lit do 
+        game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
+        task.wait()
+    end
+end))
+
+for i,v in pairs(Player.Backpack:GetChildren()) do
+    if v:IsA('Tool') then
+        v.Parent = Player.Character
+    end
+end
+            
+local T = 0
+
+repeat
+    for i,v in pairs(Player.Backpack:GetChildren()) do
+        if v:IsA('Tool') then
+            v.Parent = Player.Character
+        end
+    end
+    T = T + 1
+until T >= 35
+ 
+wait(3)
+Lit = true
+DeleteChar()
+end)
