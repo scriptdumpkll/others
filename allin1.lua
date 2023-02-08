1026f282acd715d2fbb07f6a7aa710487378620f
scriptdumpkll <113224535+scriptdumpkll@users.noreply.github.com>
2023-02-08 09:38:17 +0800

    Update allin1.lua

--- allin1.lua
+++ allin1.lua
@@ -43,83 +43,291 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptdumpkll/others/
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Crostide/cdhc/main/gui"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Crostide/Counter/main/Cash"))()
 
+repeat
+    wait()
+until game:IsLoaded()
 
-local plr = game.Players.LocalPlayer
-local Mouse = plr:GetMouse()
-        
+
+getgenv().Settings = {
+    host = 274127150,
+    fps = 2,
+    prefix = '/',
+    advertmsg = 'vouch jaredd 0995',
+}  
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/eyedr3ss/storage/main/libraries/cattoware"))()
+Library.theme.topheight = 50
+Library.theme.accentcolor = Color3.fromRGB(173, 223, 173)
+Library.theme.accentcolor2 = Color3.fromRGB(144, 238, 144)
+Library.theme.fontsize = 15
+Library.theme.titlesize = 17
+
+local Window = Library:CreateWindow("eyedress", Vector2.new(400, 300), Enum.KeyCode.RightControl)
+
+local AimingTab = Window:CreateTab("Commands")
+
+local SilentAimSection = AimingTab:CreateSector("Main Commands", "left")
+
+SilentAimSection:AddToggle('Drop', false, function(State)
+    if State then
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."drop")
+	else
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."stop")
+	end
+end)
+
+SilentAimSection:AddToggle('Delcash', false, function(State)
+    if State then
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."delcash on")
+	else
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."delcash off")
+	end
+end)
+
+SilentAimSection:AddToggle('Wallet', false, function(State)
+    if State then
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."money show")
+	else
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."money hide")
+	end
+end)
+
+
+SilentAimSection:AddToggle('Airlock', false, function(State)
+    if State then
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."airlock")
+	else
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."unlock")
+	end
+end)
+
+SilentAimSection:AddToggle('Ground', false, function(State)
+    if State then
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."ground")
+	else
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."unground")
+	end
+end)
+
+SilentAimSection:AddToggle('Hide', false, function(State)
+    if State then
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."hide")
+	else
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."unhide")
+	end
+end)
+
+SilentAimSection:AddToggle('Dance', false, function(State)
+    if State then
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."vibe")
+	else
+		local Prefix = getgenv().Settings.prefix
+		game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+		local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+		x.MessagePosted:fire(Prefix.."unvibe")
+	end
+end)
+
+local CameraLockSection = AimingTab:CreateSector("Setup", "right")
+
+CameraLockSection:AddButton('Admin', function(State)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."setup admin")
+end)
+
+CameraLockSection:AddButton('Bank', function(State)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."setup bank")
+end)
+
+CameraLockSection:AddButton('Club', function(State)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."setup club")
+end)
+
+CameraLockSection:AddButton('Train', function(State)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."setup train")
+end)
+
+CameraLockSection:AddButton('School', function(State)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."setup school")
+end)
+
+CameraLockSection:AddButton('Basketball', function(State)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."setup basketball")
+end)
+
+CameraLockSection:AddButton('Jail', function(State)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."setup jail")
+end)
+
+local TargetAimSection = AimingTab:CreateSector("Target Commands", "left")
+
+TargetAimSection:AddTextbox('Bring',  nil, function(Text)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."bring "..Text)
+end)
+
+TargetAimSection:AddTextbox('Custom Drop', nil, function(Text)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."cdrop "..Text)
+end)
+
+TargetAimSection:AddTextbox('Surround', nil, function(Text)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."to "..Text)
+end)
+
+TargetAimSection:AddTextbox('Dupe', nil, function(Text)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."dupe "..Text)
+end)
+
+TargetAimSection:AddTextbox('Say', nil, function(Text)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."say "..Text)
+end)
+
+TargetAimSection:AddTextbox('Fps', nil, function(Text)
+	local Prefix = getgenv().Settings.prefix
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("", "System")
+	local x = require(game.Players.LocalPlayer.PlayerScripts.ChatScript.ChatMain)
+	x.MessagePosted:fire(Prefix.."fps "..Text)
+end)
+
 Mouse.KeyDown:Connect(function(a)
     if a == "l" then
         if game.CoreGui:FindFirstChild("Redware") then
             game.CoreGui.Redware:Destroy()
         end
+
         Holder = Instance.new("ScreenGui")
         Frame = Instance.new("Frame")
-
+        TextLabel = Instance.new("TextLabel")
+        TextLabel_2 = Instance.new("TextLabel")
+        TextLabel_3 = Instance.new("TextLabel")
+        
         Holder.Name = "Redware"
         Holder.Parent = game.CoreGui
         Holder.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
         Holder.IgnoreGuiInset = true
-        
+
         Frame.Parent = Holder
         Frame.AnchorPoint = Vector2.new(0.5, 0.5)
-        Frame.BackgroundColor3 = Color3.fromRGB(144, 238, 144)
+        Frame.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
         Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
         Frame.Size = UDim2.new(1, 0, 1, 0)
-        setfpscap(3)
+
+        TextLabel.Parent = Frame
+        TextLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+        TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+        TextLabel.BackgroundTransparency = 1.000
+        TextLabel.Position = UDim2.new(0.5, 0, 0.49158523, 0)
+        TextLabel.Size = UDim2.new(0, 300, 0, 60)
+        TextLabel.Font = Enum.Font.GothamBlack
+        TextLabel.Text = "eyedress#0995"
+        TextLabel.TextColor3 = Color3.fromRGB(144,238,144)
+        TextLabel.TextScaled = true
+        TextLabel.TextSize = 14.000
+        TextLabel.TextWrapped = true
+
+        TextLabel_2.Parent = Frame
+        TextLabel_2.AnchorPoint = Vector2.new(0.5, 0.5)
+        TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+        TextLabel_2.BackgroundTransparency = 1.000
+        TextLabel_2.Position = UDim2.new(0.499739617, 0, 0.549961051, 0)
+        TextLabel_2.Size = UDim2.new(0, 180, 0, 40)
+        TextLabel_2.Font = Enum.Font.Nunito
+        TextLabel_2.Text = game.Players.LocalPlayer.Name
+        TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+        TextLabel_2.TextScaled = true
+        TextLabel_2.TextSize = 14.000
+        TextLabel_2.TextWrapped = true
+        
+        TextLabel_3.Parent = Frame
+        TextLabel_3.AnchorPoint = Vector2.new(0.5, 0.5)
+        TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+        TextLabel_3.BackgroundTransparency = 1.000
+        TextLabel_3.Position = UDim2.new(0.499739617, 0, 0.599961051, 0)
+        TextLabel_3.Size = UDim2.new(0, 180, 0, 40)
+        TextLabel_3.Font = Enum.Font.TitilliumWeb
+        TextLabel_3.Text = "MAIN"
+        TextLabel_3.TextColor3 = Color3.fromRGB(255, 0, 0)
+        TextLabel_3.TextScaled = true
+        TextLabel_3.TextSize = 14.000
+        TextLabel_3.TextWrapped = true
+        setfpscap(2)
     elseif a == "k" then
         setfpscap(60)
         if game.CoreGui:FindFirstChild("Redware") then
             game.CoreGui.Redware:Destroy()
         end
-    end
-end)
-
-function saymsg(msg)
-    game:service"ReplicatedStorage".DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg, 'All')
-end
-
-local Players = game:GetService("Players")
-
-function CMD(Str)
-    local Cmd = (Str:lower():split(" "))
-    if Cmd[1] == ((getgenv().Settings.prefix).."start") then
-        wait(3)
-        saymsg((getgenv().Settings.prefix).."delcash on")
-        if Cmd[2] == nil then
-            wait(3)
-            saymsg((getgenv().Settings.prefix).."setup admin")
-            wait(3)
-            saymsg((getgenv().Settings.prefix).."god")
-            wait(3)
-            saymsg((getgenv().Settings.prefix).."drop")
-        elseif Cmd[2] and not Cmd[3] then
-            wait(3)
-            saymsg((getgenv().Settings.prefix).."setup admin")
-            wait(3)
-            saymsg((getgenv().Settings.prefix).."god")
-            wait(3)
-            saymsg((getgenv().Settings.prefix).."cdrop "..Cmd[2])
-        elseif Cmd[3] and not Cmd[4] then
-            wait(3)
-            saymsg((getgenv().Settings.prefix).."setup "..Cmd[3])
-            wait(3)
-            saymsg((getgenv().Settings.prefix).."god")
-            wait(3)
-            saymsg((getgenv().Settings.prefix).."cdrop "..Cmd[2])
-        end
-    elseif Cmd[1] == ((getgenv().Settings.prefix).."stop") then
-        wait(3)
-        saymsg("/unlock")
-    elseif Cmd[1] == ((getgenv().Settings.prefix).."dp") then
-        wait(3)
-        saymsg((getgenv().Settings.prefix).."host around")
-        wait(3)
-        saymsg((getgenv().Settings.prefix).."drop")
-    end
-end
-
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.OnMessageDoneFiltering.OnClientEvent:Connect(function(Data) 
-    if Players[Data.FromSpeaker].UserId == tonumber(getgenv().Settings.host) then
-        CMD(Data.Message)
     end
 end)
